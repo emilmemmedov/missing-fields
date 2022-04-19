739d0cf38621bc2a90508112747b7ef26a84e764
emilmemmedov <emill.memmedovv@gmail.com>
2022-04-19 18:15:01 +0400

    done

---
+++ index.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const fields = [
+    'shortName',
+];
+
+let payload  = [];
+
+axios.get('https://gw.selinatech.com/locations/api/locations/').then((data) =>{
+    handle(data.data);
+    console.log(payload);
+})
+
+function handle(json){
+    for (let i = 0; i < fields.length; i++){
+        payload.push({
+            'missing_field': fields[i],
+            'locations': JSON.stringify(findMissingField(fields[i], json))
+        })
+    }
+}
+
+function findMissingField(field, json){
+    let missingFieldsLocation = []
+    for (let i = 0; i < json.length; i++){
+        if (json[i][field] == null || json[i][field] === ""){
+            // missingFieldsLocation.push(json[i]['id'])
+            missingFieldsLocation.push({
+                "id": json[i]['id'],
+                "name": json[i]['name'],
+            })
+        }
+    }
+    return missingFieldsLocation;
+}
